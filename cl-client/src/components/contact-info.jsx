b1b8eb73035025c964f599bc7f6c1ed6657057e8
TashaLapitanova <natalia@farmy.ch>
2021-10-24 21:27:44 +0200

    display contact changes history

--- cl-client/src/components/contact-info.jsx
+++ cl-client/src/components/contact-info.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Row, Col, Button} from 'react-bootstrap';
 
 export const ContactInfo = (props) => {
 
     const {first_name, last_name, email, phone_number, comment, change_log} = props.contact;
+    const [showInfo, setShowInfo] = useState(true);
 
     function prepareEdit() {
         props.setContactToEdit(props.contact);
@@ -13,7 +14,8 @@ export const ContactInfo = (props) => {
         props.setContactToDelete(props.contact);
     };
 
-    return <Card.Body>
+    function renderContactInfo() {
+        return <Card.Body>
                 <Card.Title>{last_name}, {first_name}</Card.Title>
                 {comment && <p className="text-muted">{comment}</p>}
                 <Card.Text>
@@ -27,8 +29,37 @@ export const ContactInfo = (props) => {
                     </Row>
                 </Card.Text>
                 <div>
-                    <Button onClick={()=>prepareEdit()}>Edit</Button>
-                    <Button onClick={()=>prepareDelete()}>Delete</Button>
+                    <div>
+                        <Button onClick={()=>prepareEdit()}>Edit</Button>
+                        <Button onClick={()=>prepareDelete()}>Delete</Button>
+                    </div>
+                    <Button onClick={()=>setShowInfo(false)}>History</Button>
                 </div>
             </Card.Body>
+    };
+
+    function prettifyDate(dateStr) {
+        return new Date(dateStr).toDateString().slice(4,15);
+    };
+
+    function renderContactHistory() {
+        return <Card.Body>
+                <Card.Title>Contact history log</Card.Title>
+                <Card.Text>
+                    {change_log.map((r, i) => {
+                        return  <Row key={i}>
+                            <Col md={4}>{prettifyDate(r.created_at)}</Col>
+                            <Col md={8}>{r.details}</Col>
+                        </Row>
+                    })}
+                </Card.Text>
+                <div>
+                    <Button onClick={()=> setShowInfo(true)}>Close history</Button>
+                </div>
+            </Card.Body>
+    };
+
+    return <Card>
+                {showInfo ? renderContactInfo() : renderContactHistory()}
+            </Card>
 };
